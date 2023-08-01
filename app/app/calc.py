0ce1ef0f7b7f13a3f20168a77d5509b926a98270
NangPwintPHoo <pwintphoo@isgm2.com>
2023-08-02 00:01:52 +0630

    Fixing Whitespace

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,9 +1,8 @@
 """
 Calculte  Functions
 """
+def add(x, y):
+    return x + y
 
-def add(x,y):
-    return x + y 
-
-def subtract(x,y):
-    return y - x 
+def subtract(x, y):
+    return y - x
--- app/app/tests.py
+++ app/app/tests.py
@@ -5,12 +5,12 @@ class CalcTests(SimpleTestCase):
     
     def test_add_numbers(self):
 
-        res = calc.add(5,6)
+        res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
 
     def test_substract_numbers(self):
 
-        res = calc.subtract(10,15)
+        res = calc.subtract(10, 15)
 
-        self.assertEqual(res,5)
+        self.assertEqual(res, 5)
